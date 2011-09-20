901f9650d11fb55d1466ac5a14b7c74e72e015bb
Timo Sand <timo.sand@kapsi.fi>
2011-09-20 20:54:48 +0300

    Getting there, now we have traversal, but still no heuristics

--- week2/a_star.py
+++ week2/a_star.py
@@ -6,29 +6,32 @@ from node import Node
 sys.path.append('../')
 from priority_dict import *
 
+path = []
+
 def search(beginning, goal):
-  node_list = priority_dict();
-  node = Node(beginning[0], beginning[1])
-  node_list[node.stop()] = node.time()
+  node_list = priority_dict()
+  node_list[beginning[0]] = beginning[1]
   while len(node_list) > 0:
     print"Solmulista: " + ", ".join([str((k, v)) for k,v in node_list.items()])
     node = node_list.smallest()
-    print "Solmu: " + node.name + ", " + str(node_list[node])
+    time = node_list[node]
+    print "Path: " + str(path)
+    path.append((node.name, time))
+    print "Solmu: " + node.name + ", " + str(time)
     node_list.pop_smallest()
     if node.name == goal:
       return("Ratkaisu: ", node)
-    node.set_explored()
-    node_list = _add_a_star(node, node_list)
+    node_list = _add_a_star(neighbors(node, time), node_list)
   return ("Ei ratkaisua", node)
 
-def _add_a_star(popped_node, nodes):
+def _add_a_star(neighs, nodes):
   return_prioq = nodes
-  neighbors = neighbors(popped_node)
-  for node in neighbors:
-    if is_smallest(node.stop(), node.time(), return_prioq):
-      node.set_reached(popped_node)
-    if not node.explored:
-      return_prioq[node.stop()] = node.time()
+  print neighs
+  for stop, time in neighs:
+    if stop.name not in [foo[0] for foo in path]:
+      return_prioq[stop] = time
+      if is_smallest(stop, time, return_prioq):
+        path.append((stop, time))
   return return_prioq
 
 def is_smallest(stop, time, node_estimate_list):
@@ -36,31 +39,39 @@ def is_smallest(stop, time, node_estimate_list):
     return time < node_estimate_list[stop]
   return True
 
-def neighbors(node):
+def neighbors(stop, time):
   neighbor_nodes = []
-  stop = node.stop()
-  time = node.time()
-  for line in [trans[0] for trans in node.transportations]:
+  for line in [trans[0] for trans in stop.transportations]:
     wait_time = calculate_wait_time(line,stop,time)
     next_stop = get_next_stop(line, stop)
-    if next_stop is not None:
+    if next_stop is not None and next_stop.name not in [foo[0] for foo in path]:
       travel_time = calculate_travel_time(line, stop, next_stop)
-      neighbor_nodes.append(Node(next_stop, time+wait_time+travel_time))
+      neighbor_nodes.append((next_stop, time+wait_time+travel_time))
+  print "NN: " + str(neighbor_nodes)
   return neighbor_nodes
 
 def calculate_wait_time(line_name, stop, time):
   next_time = [x[1] for x in stop.transportations if x[0] == line_name].pop()[1]
   while time > next_time:
-    next_time + 10
+    next_time += 10
   return time - next_time
 
 def get_next_stop(line_name, stop):
-  next_stop = [[bar.name for bar in foo.transportations if bar.name == line_name] for foo in stop.neighbors].pop()
-  return next_stop
+  for neighbor in stop.neighbors:
+    for trans_name, trans in neighbor.transportations:
+      if trans_name == line_name and stop is not neighbor:
+        return neighbor
+
 
 def calculate_travel_time(line_name, prev_stop, next_stop):
-  prev_time = [x[1] for x in prev_stop.transportations if x[0] == line_name].pop()[1]
-  next_time = [x[1] for x in next_stop.transportations if x[0] == line_name].pop()[1]
+  for trans_name, trans in prev_stop.transportations:
+    if trans_name == line_name:
+      prev_time = trans[1]
+      break
+  for trans_name, trans in next_stop.transportations:
+    if trans_name == line_name:
+      next_time = trans[1]
+      break
   return next_time - prev_time
 
 def print_route(last_node):
